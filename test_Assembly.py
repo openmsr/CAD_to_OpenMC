d84896d94a98a49ae7fc42f6fb68c82157e59ce8
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-04-26 10:20:45 +0200

    add another test WIP

--- test_Assembly.py
+++ test_Assembly.py
@@ -1,5 +1,15 @@
 import pytest
-import assembly
+import assembly as asmb
+import pathlib as pl
 
 def test_test():
     assert 1==1
+
+@pytest.mark.parametrize("infile",['1pin_hollow_contact.step'])
+def test_assembly(infile="in.step"):
+    file=pl.Path(infile)
+    a=asmb.Assembly()
+    a.import_stp_file(file)
+    string=a.export_stl(file.with_suffix('.stl'))
+    print(string)
+    assert string == file.with_suffix('.stl')
