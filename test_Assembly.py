10a1eeab392ef6a3b406deaa2755f8806c37658b
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-04-27 17:45:27 +0200

    setup unit test

--- test_Assembly.py
+++ test_Assembly.py
@@ -5,11 +5,15 @@ import pathlib as pl
 def test_test():
     assert 1==1
 
-@pytest.mark.parametrize("infile",['1pin_hollow_contact.step'])
-def test_assembly(infile="in.step"):
+@pytest.mark.parametrize("infile",['examples/7pins.step'])
+def test_brep_export(infile: str="in.step"):
     file=pl.Path(infile)
-    a=asmb.Assembly()
-    a.import_stp_file(file)
-    string=a.export_stl(file.with_suffix('.stl'))
-    print(string)
-    assert string == file.with_suffix('.stl')
+    a=asmb.Assembly(verbose=2)
+    a.stp_files=[infile]
+    a.import_stp_file()
+    ofile='out.brep'
+    h5m='out.h5m'
+    assert a.export_brep(ofile)
+    a.brep_to_h5m(ofile, h5m_filename=h5m, samples=20, min_size_mesh=0.5, max_size_mesh=10)
+    h5p=pl.Path(h5m)
+    assert h5p.exists()
