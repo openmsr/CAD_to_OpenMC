7c74b9cc1defd141fbfdb3b046b88b5e3200a564
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-06-29 15:01:33 +0200

    implementing flake8 linter style hints

--- test_Assembly.py
+++ test_Assembly.py
@@ -2,15 +2,15 @@ import pytest
 import assembly as asmb
 import pathlib as pl
 
+
 def test_brep_export():
-    infile='examples/7pins.step'
-    file=pl.Path(infile)
-    a=asmb.Assembly(verbose=2)
-    a.stp_files=[infile]
+    infile = 'examples/7pins.step'
+    a = asmb.Assembly(verbose = 2)
+    a.stp_files = [infile]
     a.import_stp_file()
-    ofile='out.brep'
-    h5m='out.h5m'
+    ofile = 'out.brep'
+    h5m = 'out.h5m'
     assert a.export_brep(ofile)
-    a.brep_to_h5m(ofile, h5m_filename=h5m, samples=20, min_size_mesh=0.5, max_size_mesh=10)
+    a.brep_to_h5m(ofile, h5m_filename = h5m, samples = 20, min_size_mesh = 0.5, max_size_mesh = 10)
     h5p=pl.Path(h5m)
     assert h5p.exists()
