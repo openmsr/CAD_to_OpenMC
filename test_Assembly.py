bd70ba264778f164b3e908bbd52884a54765606e
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-07-07 08:54:42 +0200

    wip: add  skeleton

--- test_Assembly.py
+++ test_Assembly.py
@@ -2,15 +2,25 @@ import pytest
 import src.h5massembly.assembly as asmb
 import pathlib as pl
 
+class TestRun()
 
-def test_brep_export():
+  def __init__(self):
+    pass
+
+  def test_brep_export(self):
     infile = 'examples/7pins.step'
-    a = asmb.Assembly(verbose=2)
-    a.stp_files = [infile]
-    a.import_stp_file()
-    ofile = 'out.brep'
-    h5m = 'out.h5m'
+    self.a = asmb.Assembly(verbose=2)
+    self.a.stp_files = [infile]
+    self.a.import_stp_file()
+    self.ofile = 'out.brep'
+    self.h5m = 'out.h5m'
     assert a.export_brep(ofile)
-    a.brep_to_h5m(ofile, h5m_filename=h5m, samples=20, min_size_mesh=0.5, max_size_mesh=10)
+
+  def test_gmsh(self):
+    brep_to_h5m(ofile, h5m_filename=h5m, samples=20, min_size_mesh=0.5, max_size_mesh=10)
     h5p = pl.Path(h5m)
     assert h5p.exists()
+
+
+  def test_stl(self):
+    pass
