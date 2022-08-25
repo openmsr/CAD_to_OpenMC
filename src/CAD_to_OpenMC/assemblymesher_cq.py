1aa06feb398df3e18a5fd31404b7d2a0791aae96
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-08-25 10:47:36 +0200

    WIP: mesh volume faces one by one instead
    towards allowing refinement of each face without ruining
    merging operation

--- src/CAD_to_OpenMC/assemblymesher_cq.py
+++ src/CAD_to_OpenMC/assemblymesher_cq.py
@@ -2,6 +2,7 @@ import cadquery2 as cq
 import subprocess as sp
 import pathlib as pl
 import hashlib as hl
+from .stl_utils import *
 
 class MesherCQSTL:
   def __init__(self, tolerance, angular_tolerance, default, refine, entities):
@@ -12,6 +13,8 @@ class MesherCQSTL:
     self.refine=refine
 
   def generate_stls(self):
+    self._mesh_surfaces()
+    return
     #created a cq-compund from list of entities
     for i,e in enumerate(self.cq_mesher_entities):
       j=i+1
@@ -52,31 +55,32 @@ class MesherCQSTL:
     gmsh.write( str(stlp) )
     gmsh.finalize()
 
-
   def _mesh_surfaces(self):
     #loop over all surfaces in all entities
     #and generate meshes (refined or otherwise)
     facehashtable={}
     for i,e in enumerate(self.entities):
-      j=i+1
-      volname= f"volume_{j}.stl"
+      volname= f"volume_{i+1}.stl"
       k=0
-      for f in e.solid.Faces():
-
+      volumefaces=[]
+      for j,f in enumerate(e.solid.Faces()):
         m=hl.sha256()
-        hh=m.update(f)
-        if hh not in keys(facehashtable):
-          facehastable.update(hh,facename)
+        hh=m.update(bytes(repr(f),encoding='ascii'))
+        if hh in facehashtable.keys():
+          #surface is in table - use that file for this volume
+          volumefaces.append(facehastable[hh])
         else:
-
-        #export to stl
-        #refine?
+          facename=f'vol_{i+1}_face{j}.stl'
+          cq.exporters.export(f,facename, exportType="STL", tolerance=self.tolerance, angularTolerance=self.angular_tolerance)
+          if(True or self.verbose>1):
+            print(f"INFO: cq export to file {facename}")
+          volumefaces.append(facename)
+          #surface is not in table - we need to mesh (and possibly remesh) it
+          #and put it into the main facetable as well as the local table for this volume.
+          #refine?
           #if so reimport
-
-
-
-
-
+      merge_stl(volname, volumefaces,of='bin')
+      e.stl=volname
 
 class MesherCQSTLBuilder:
   def __init__(self):
