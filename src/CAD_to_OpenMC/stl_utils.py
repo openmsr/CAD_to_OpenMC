0ab673d125cfeff77630c183b23ab3d04eaaa96a
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-08-29 17:28:33 +0200

    add code to extract vertices and triangles from raw buffer

--- src/CAD_to_OpenMC/stl_utils.py
+++ src/CAD_to_OpenMC/stl_utils.py
@@ -2,6 +2,7 @@
 a set of utility functions that deal with stl-files
 """
 import struct
+import numpy as np
 
 def merge_stl(dest,src, of='binary'):
   """
@@ -28,6 +29,17 @@ def merge_stl(dest,src, of='binary'):
       outputbuffer.extend(buffer)
       ntot+=n
   write_stl(dest, outputbuffer,of=of,header=outputbh)
+
+def read_stl(src):
+  infp=open(src,"rb")
+  magic_bytes=infp.read(5)
+  if (magic_bytes[:5].decode('ascii')=='solid'):
+    infp.close()
+    infp=open(s,'r')
+    buffer,n = read_stl_ascii(infp)
+  else:
+    buffer,n = read_stl_bin(infp)
+  return buffer,n
 
 def read_stl_ascii(infp):
   line=infp.readline()
@@ -77,11 +89,45 @@ def write_stl_bin(fname, faces, header=b'', color=None):
     outfp.write(b'\x00'*(80-len(header)))
     outfp.write(struct.pack('I',len(faces)))
     for facet in faces:
-      print(len(facet),*facet)
       outfp.write( struct.pack( '12f',*facet ) )
       #if necessary pad with the attribute byte count (15-bit color) - for now set to 0
       if (len(facet)==12):
         outfp.write(struct.pack('H',0))
+
+def buffer2vertices(buffer):
+  """take a raw buffer as returned by the functions above and extract only the vertices from it"""
+  # This amounts to extracting only the last 9 elements (i.e. disregard the normals),
+  # stacking them vertically to get a list of 3d-coordinates as rows.
+  # and return a list of unique rows
+  npbuf=np.array(buffer)
+  uqe=np.unique( np.vstack( (npbuf[:,3:6],npbuf[:,6:9],npbuf[:,9:12]) ),axis=0)
+  return uqe
+
+def find_vertex(v,vertices):
+  logic=(np.equal(vertices,v)).all(1)
+  idx=np.flatnonzero(logic)[0]
+  return idx
+
+def buffer2triangles(buffer,vertices=None):
+  if vertices is None:
+    vertices=buffer2vertices(buffer)
+  npbuf=np.array(buffer)[:,3:]
+  triangles=None
+  for i in range(npbuf.shape[0]):
+    v1=npbuf[i,0:3]
+    v2=npbuf[i,3:6]
+    v3=npbuf[i,6:9]
+    idx1=find_vertex(v1,vertices)
+    idx2=find_vertex(v2,vertices)
+    idx3=find_vertex(v3,vertices)
+    if triangles is None:
+      triangles=np.array([[idx1,idx2,idx3]])
+    else:
+      triangles=np.vstack((triangles,[idx1,idx2,idx3]))
+  return triangles
+
+
+
 
 if __name__=='__main__':
     import sys
