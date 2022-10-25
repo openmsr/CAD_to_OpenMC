505b74b1de4658047a75e5bb696d255033e4425a
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-10-25 11:54:36 +0200

    rewrite to parallel cq-meshing and refinement
    This uses python multiprocessing Pool, which in turn meant
    we need to have required parameters as class parameters and methods.
    This to avoid serialization/pickling of objects which is not allowed.
    Limitation is that you can then only have a single instance of the mesher
    object, but this is reasonable.

--- src/CAD_to_OpenMC/assemblymesher_cq.py
+++ src/CAD_to_OpenMC/assemblymesher_cq.py
@@ -2,19 +2,45 @@ import cadquery as cq
 import subprocess as sp
 import pathlib as pl
 import hashlib as hl
+import multiprocessing.pool as mp
+
 from .stl_utils import *
 from . import meshutils
 
+
+
+
+#following pattern here: https://thelaziestprogrammer.com/python/multiprocessing-pool-a-global-solution
+#to enable multiprocessing meshings
+
 class MesherCQSTL:
-  def __init__(self, tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default, refine, entities):
-    self.tolerance=tolerance
-    self.angular_tolerance=angular_tolerance
-    self.entities=entities
+  #these need to be class attributes to avoid pickling when spawning a multiprocessing-pool
+  cq_mesher_entities=None
+  cq_mesher_tolerance=None
+  cq_mesher_ang_tolereance=None
+  cq_mesher_min_mesh_size=None
+  cq_mesher_max_mesh_size=None
+
+  def __init__(self, tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default, refine, threads, entities):
+    self._set_meshpars(tolerance,angular_tolerance,min_mesh_size,max_mesh_size)
     self.verbose=2
     self.refine=refine
+    self._set_entities(entities)
     self.default=default
     self.min_mesh_size=min_mesh_size
     self.max_mesh_size=max_mesh_size
+    self.threads=threads
+
+  @classmethod
+  def _set_meshpars(cls,tol,ang_tol, min_sz, max_sz):
+    cls.cq_mesher_tolerance=tol
+    cls.cq_mesher_ang_tolerance=ang_tol
+    cls.cq_mesher_min_mesh_size=min_sz
+    cls.cq_mesher_max_mesh_size=max_sz
+
+  @classmethod
+  def _set_entities(cls,entities):
+    cls.cq_mesher_entities=entities
 
   def generate_stls(self):
     self._mesh_surfaces()
@@ -30,16 +56,18 @@ class MesherCQSTL:
       if(self.refine):
         self._refine_stls(e.stl)
 
-  def _refine_stls(self,stl):
+  @classmethod
+  def _refine_stls(cls,stl,refine):
     try:
-      if(self.refine.startswith('mmg')):
-        self._mmgs_refine_stls(stl)
+      if(refine.startswith('mmg')):
+        cls._mmgs_refine_stl(stl)
       else:
         print(f'ERROR: Unknown mesh refinement algorithm: {self.refine}')
     except:
-        self._mmgs_refine_stls(stl)
+        cls._mmgs_refine_stl(stl)
 
-  def _mmgs_refine_stls(self,stl):
+  @classmethod
+  def _mmgs_refine_stl(cls,stl):
     #call mmgs to refine the mesh
     try:
       cp=sp.run(['mmgs_O3','--help'],capture_output=True)
@@ -53,53 +81,9 @@ class MesherCQSTL:
     tris=buffer2triangles(buf,verts)
     edges=np.array(meshutils.find_edges(tris))
     meshutils.write_dotmesh(str(stlp.with_suffix('.mesh')),verts,tris,edges=edges,required_edges='all')
-    cp=sp.run(['mmgs_O3','-hmin',f'{self.min_mesh_size}','-hmax',f'{self.max_mesh_size}','-optim','-in',stlp.with_suffix('.mesh'),'-out',stlp.with_suffix('.o.mesh')], capture_output=True)
+    cp=sp.run(['mmgs_O3','-hmin',f'{cls.cq_mesher_min_mesh_size}','-hmax',f'{cls.cq_mesher_max_mesh_size}','-optim','-in',stlp.with_suffix('.mesh'),'-out',stlp.with_suffix('.o.mesh')], capture_output=True)
     print(cp.stdout)
 
-    import gmsh
-    gmsh.initialize()
-    gmsh.open(str(stlp.with_suffix('.o.mesh')))
-    gmsh.write( str(stlp) )
-    gmsh.finalize()
-
-  def _refine_volumefaces(self,stls,output_stl='out.stl'):
-    #refine a volume consistng of a set of stl-files which is expected to contains its parts
-    all_verts=None
-    for i,vf in enumerate(stls):
-      print(f'processing: {vf}')
-      #if this is reuse of an already refined surface we should extract the information from there instead.
-      #check if the file exists
-      vfp=pl.Path(vf)
-      if False and vfp.with_suffix('.o.mesh').exists():
-        print(f'found refined file {str(vfp.with_suffix(".o.mesh"))} extracting information from that')
-        import gmsh
-        gmsh.initialize()
-        gmsh.open(str(vfp.with_suffix('.o.mesh')))
-        gmsh.write(str(vfp.with_suffix('.tmp.stl')))
-        gmsh.finalize()
-
-      buf,n=read_stl(vf)
-      verts=buffer2vertices(buf)
-      triangles=buffer2triangles(buf,verts)
-      if (all_verts is None):
-        all_verts=verts
-        all_triangles=triangles
-        all_tlabels=(i+1)*np.ones((triangles.shape[0]),dtype='uint')
-        all_vlabels=1*np.ones((verts.shape[0]),dtype='uint')
-        vertex_count=verts.shape[0]
-      else:
-        all_verts=np.vstack((all_verts,verts))
-        #these new triangle labels need an offset of the current vertex count
-        all_triangles=np.vstack((all_triangles,triangles+vertex_count))
-        all_tlabels=np.append(all_tlabels,(i+1)*np.ones((triangles.shape[0]),dtype='uint'))
-        all_vlabels=np.append(all_vlabels,1*np.ones((verts.shape[0]),dtype='uint'))
-        vertex_count+=verts.shape[0]
-    stlp=pl.Path(output_stl)
-    meshutils.write_dotmesh(stlp.with_suffix('.mesh'),all_verts,all_triangles,vertex_labels=all_vlabels, triangle_labels=all_tlabels)
-    meshutils.write_dummy_dotsol(stlp.with_suffix('.sol'),all_verts.shape[0])
-
-    cp=sp.run(['mmgs_O3','-ls','-sol',stlp.with_suffix('.sol'),'-in',stlp.with_suffix('.mesh'),'-out',stlp.with_suffix('.o.mesh'),'-keep-ref'], capture_output=True)
-    print(cp.stdout)
     import gmsh
     gmsh.initialize()
     gmsh.open(str(stlp.with_suffix('.o.mesh')))
@@ -110,34 +94,45 @@ class MesherCQSTL:
     #loop over all surfaces in all entities
     #and generate meshes (refined or otherwise)
     facehashtable={}
-    for i,e in enumerate(self.entities):
+    for i,e in enumerate(self.cq_mesher_entities):
       volname= f"volume_{i+1}.stl"
       k=0
+      mpargs=[(j,i,facehashtable,self.refine) for j,f in enumerate(e.solid.Faces())]
+      pl=mp.Pool(processes=4)
+      output=pl.starmap(self._mesh_single, mpargs)
+      print(output)
+      print(len(output))
       volumefaces=[]
-      for j,f in enumerate(e.solid.Faces()):
-        hh=hash(f)
-        if hh in facehashtable.keys():
-          #surface is in table - use that file for this volume
-          volumefaces.append(facehashtable[hh])
-          print(f'reusing {hh} {facehashtable[hh]}')
-        else:
-          facename=f'vol_{i+1}_face{j}.stl'
-          facehashtable[hh]=facename
-          f.exportStl(facename, tolerance=0.1, angularTolerance=self.angular_tolerance, ascii=True)
-          if(True or self.verbose>1):
-            print(f"INFO: cq export to file {facename}")
-          volumefaces.append(facename)
-          if (self.refine):
-            self._refine_stls(facename)
+      for o in output:
+        facehashtable[o[0]]=o[1]
+        volumefaces.append(o[1])
       #merge the stls to a single .stl
       merge_stl(volname, volumefaces,of='bin')
       e.stl=volname
+
+  @classmethod
+  def _mesh_single(cls, fid, vid, facehashtable, refine):
+    f=cls.cq_mesher_entities[vid].solid.Faces()[fid]
+    hh=hash(f)
+    if hh in facehashtable.keys():
+      #surface is in table - use that file for this volume
+      print(f'reusing {hh} {facehashtable[hh]}')
+      return(hh,facehashtable[hh])
+    else:
+      facename=f'vol_{vid+1}_face{fid}.stl'
+      facehashtable[hh]=facename
+      f.exportStl(facename, tolerance=cls.cq_mesher_tolerance, angularTolerance=cls.cq_mesher_ang_tolerance, ascii=True)
+      if(True or self.verbose>1):
+        print(f"INFO: cq export to file {facename}")
+      if (refine):
+        cls._refine_stls(facename,refine)
+      return(hh,facename)
 
 class MesherCQSTLBuilder:
   def __init__(self):
     self._instance = None
 
-  def __call__(self, tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default, refine, entities, **_ignored):
+  def __call__(self, tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default, refine, threads, entities, **_ignored):
     if not self._instance:
-      self._instance = MesherCQSTL(tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default,refine, entities)
+      self._instance = MesherCQSTL(tolerance, angular_tolerance, min_mesh_size, max_mesh_size, default,refine, threads, entities)
     return self._instance
