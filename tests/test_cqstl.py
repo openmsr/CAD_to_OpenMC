8399e1b73d82e858d3bd4b5dd66bc9e17e091732
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-10-28 15:54:38 +0200

    improve test_suite

---
+++ tests/test_cqstl.py
@@ -0,0 +1,26 @@
+import pytest
+from tests.testRun import *
+
+class TestCqSTL(TestRun):
+  def __init__(self):
+    super().__init__()
+
+  def run(self):
+    print(f'stl: {self.a.stp_files})')
+    print(self.h5m)
+    #self.a.solids_to_h5m(backend='stl')
+    h5p = pl.Path('out_cqstl.h5m')
+    self.a.solids_to_h5m(backend='stl',h5m_filename=str(h5p))
+    assert h5p.exists()
+    assert self.is_validh5m(h5p)
+    self.cleanup()
+
+  def cleanup(self):
+    super().cleanup()
+    pwd=pl.Path('.')
+    for v in pwd.glob("vol*_face*"):
+      v.unlink()
+
+def testcq():
+  t = TestCqSTL()
+  t.run()
