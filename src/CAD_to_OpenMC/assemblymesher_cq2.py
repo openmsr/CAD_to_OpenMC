dc8ba241ce383dd56b13c1de77796045ed7813d0
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-10-04 14:33:59 +0200

    faceid is now unique for each face

--- src/CAD_to_OpenMC/assemblymesher_cq2.py
+++ src/CAD_to_OpenMC/assemblymesher_cq2.py
@@ -77,9 +77,10 @@ class MesherCQSTL2(assemblymesher):
     else:
       #manager=mp.Manager()
       face_hash_table=manager.dict()
-
+    k=0
     for i,e in enumerate(self.cq_mesher_entities):
-      mpargs.extend([(j,i,self.refine,hash(f), face_hash_table) for j,f in enumerate(e.solid.Faces())])
+      mpargs.extend([(k+j,j,i,self.refine,hash(f), face_hash_table) for j,f in enumerate(e.solid.Faces())])
+
     #we have a set of mesh jobs - scatter those
     if (single_thread_override or self.threads==1):
       output=[]
@@ -102,7 +103,7 @@ class MesherCQSTL2(assemblymesher):
     return stls
 
   @classmethod
-  def _mesh_single(cls, fid, vid, refine, hh, faceHash):
+  def _mesh_single(cls, global_fid, fid, vid, refine, hh, faceHash):
     f=cls.cq_mesher_entities[vid].solid.Faces()[fid]
     if hh in faceHash.keys():
       #surface is in table - simply add the vid to the hash-table
@@ -112,7 +113,7 @@ class MesherCQSTL2(assemblymesher):
         print(f'INFO: mesher reusing {hh} {faceHash[hh][1]}')
       return(hh,faceHash[hh])
     else:
-      facefilename=f'vol_{vid+1}_face{fid}.stl'
+      facefilename=f'vol_{vid+1}_face{global_fid:04}.stl'
       with lock:
         faceHash[hh]=[facefilename,manager.list([vid])]
       f.exportStl(facefilename, tolerance=cls.cq_mesher_tolerance, angularTolerance=cls.cq_mesher_ang_tolerance, ascii=True)
