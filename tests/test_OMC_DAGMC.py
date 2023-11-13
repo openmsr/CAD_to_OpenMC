c372ef2db197deeb7c3fd57f155a2c60dcb09c07
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-11-13 17:04:12 +0100

    fix order of indeps. to be r,phi,z

--- tests/test_OMC_DAGMC.py
+++ tests/test_OMC_DAGMC.py
@@ -38,7 +38,7 @@ def test_h5m_neutronics_tors():
   o = OMC_DAGMC_harness('examples/toroids.step')
   # override source spatial distribution
   o.tt.settings.source.space=openmc.stats.CylindricalIndependent(
-    openmc.stats.Discrete([100],[1.0]),openmc.stats.Discrete([0.0],[1.0]), openmc.stats.Uniform(0.0,2.0*math.pi)
+    openmc.stats.Discrete([100],[1.0]), openmc.stats.Uniform(0.0,2.0*math.pi), openmc.stats.Discrete([0.0],[1.0])
   )
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.run()
