fb546f40e013f4beff8c0c09facad9a60f34d15f
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-11-15 15:05:40 +0100

    merge is not called by solids...

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -13,9 +13,12 @@ class HarnessTrans(HarnessRun):
     print(f'stl: {self.a.stp_files})')
     self.a.import_stp_files(**kwargs)
 
+    if merge:
+      self.merge()
+
     #self.a.solids_to_h5m(backend='stl')
     h5p = pl.Path('out_transformed.h5m')
-    self.a.solids_to_h5m(backend='stl',merge=merge,h5m_filename=str(h5p))
+    self.a.solids_to_h5m(backend='stl',h5m_filename=str(h5p))
     assert h5p.exists()
     assert self.is_validh5m(h5p)
     self.cleanup()
