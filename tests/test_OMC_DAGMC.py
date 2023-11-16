5231e17ed0a2636995139a5b49974fd7502d888c
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-11-16 16:22:08 +0100

    fix path + cosmetics

--- tests/test_OMC_DAGMC.py
+++ tests/test_OMC_DAGMC.py
@@ -12,7 +12,7 @@ class OMC_DAGMC_harness(HarnessRun):
   def __init__(self, step):
     self.path = pathlib.Path(step)
     self.h5m = self.path.with_suffix('.h5m')
-    self.nuclear_lib = pathlib.Path('tests/nuclear_data_testlib/cross_sections.xml').absolute()
+    self.nuclear_lib = pathlib.Path('nuclear_data_testlib/cross_sections.xml').absolute()
     self.aa = ab.Assembly([str(self.path)], verbose = 2)
     self.tt = DAGMC_template(self.h5m)
 
@@ -25,12 +25,12 @@ class OMC_DAGMC_harness(HarnessRun):
     self.cleanup()
 
 def test_h5m_neutronics_p1():
-  o=OMC_DAGMC_harness('examples/pincell1.step')
+  o = OMC_DAGMC_harness('examples/pincell1.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.run()
 
 def test_h5m_neutronics_p2():
-  o=OMC_DAGMC_harness('examples/pincell2.step')
+  o = OMC_DAGMC_harness('examples/pincell2.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.run()
 
