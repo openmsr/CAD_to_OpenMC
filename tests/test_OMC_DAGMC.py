be28ac3924e184a98b58c81f779bc138b5d194cd
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2024-02-05 17:34:47 +0100

    cleanup - move steps to subdir

--- tests/test_OMC_DAGMC.py
+++ tests/test_OMC_DAGMC.py
@@ -25,19 +25,19 @@ class OMC_DAGMC_harness(HarnessRun):
     self.cleanup()
 
 def test_h5m_neutronics_p1():
-  o = OMC_DAGMC_harness('examples/pincell1.step')
+  o = OMC_DAGMC_harness('examples/step_files/pincell1.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.tt.results={'keff':(0.07944,0.00070)}
   o.run()
 
 def test_h5m_neutronics_p2():
-  o = OMC_DAGMC_harness('examples/pincell2.step')
+  o = OMC_DAGMC_harness('examples/step_files/pincell2.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.tt.results={'keff':(0.07786,0.0008)}
   o.run()
 
 def test_h5m_neutronics_tors():
-  o = OMC_DAGMC_harness('examples/toroids.step')
+  o = OMC_DAGMC_harness('examples/step_files/toroids.step')
   # override source spatial distribution
   o.tt.settings.source.space=openmc.stats.CylindricalIndependent(
     openmc.stats.Discrete([100,85,77.5],[1.0/3.0,1.0/3.0, 1.0/3.0]), openmc.stats.Uniform(0.0,2.0*math.pi), openmc.stats.Discrete([0.0],[1.0])
@@ -47,13 +47,13 @@ def test_h5m_neutronics_tors():
   o.run()
 
 def test_h5m_neutronics_spheroids():
-  o = OMC_DAGMC_harness('examples/spheroids.step')
+  o = OMC_DAGMC_harness('examples/step_files/spheroids.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.tt.results={'keff':(1.39082,0.01622)}
   o.run()
 
 def test_h5m_neutronics_ellipsoids():
-  o = OMC_DAGMC_harness('examples/oblate_ellipsoids.step')
+  o = OMC_DAGMC_harness('examples/step_files/oblate_ellipsoids.step')
   openmc.config['cross_sections']=str(o.nuclear_lib)
   o.tt.results={'keff':(1.05396,0.01514)}
   o.run()
