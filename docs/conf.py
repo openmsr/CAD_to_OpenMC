8763980b8c90d4e4d62fc3f661c08403da4b4e24
erkn <erik@vonbrun.org>
2025-03-05 15:56:50 +0100

    moving index file around

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ project = "CAD_to_OpenMC Project"
 copyright = "2025, CAD_to_OpenMC-team"
 author = "ebknudsen the Docs core team"
 
-master_doc = 'source/index'
+master_doc = 'index'
 
 extensions = [
     "sphinx.ext.duration",
